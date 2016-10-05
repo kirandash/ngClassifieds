74db443e3a7e52c1218a70865aa13d241fd56a3b
kirandash <thekirankumardash@gmail.com>
2016-10-05 20:37:53 +0530

    04 - 02 Building first controller

---
+++ components/classifieds.ctr.js
@@ -0,0 +1,10 @@
+(function(){
+
+	"use strict";
+
+	angular
+		.module("ngClassifieds"); // reference existing module - not creating new module
+		.controller("classifiedsCtrl", function(){
+
+		}); // controller defined - MVC
+})();
