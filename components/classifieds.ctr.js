f885ab0217a60ea0d2a120ff4af698b6cbbf09cc
kirandash <thekirankumardash@gmail.com>
2016-10-07 17:00:36 +0530

    05 - 02 002 The Toolbar and Classified Card

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -9,14 +9,11 @@
 		.controller("classifiedsCtrl", function($scope){
 			
 			// property defined
-			$scope.name = {
-				first: 'Kiran',
-				last: 'Dash'
-			};
-
-			$scope.username = 'kirandash';
-
-			$scope.message = 'Hello World';
+			$scope.classified = {
+				title: "First Item",
+				price: "$1,00,000",
+				description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ac arcu sit amet lacus ullamcorper tincidunt. Nulla placerat ex nulla, ut tempor leo lacinia non. Morbi pharetra sagittis ante. Sed id ornare enim. Aenean dictum dignissim justo, nec convallis sapien aliquet a. Maecenas in justo vulputate, sagittis lectus facilisis, consectetur velit. Proin quis viverra lorem. Donec tempus ante vitae lacus feugiat tempus. Praesent et hendrerit turpis. Etiam non nisi felis."
+			}
 
 		}); // controller defined - MVC
 })();
