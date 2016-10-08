2aa2dd41c4d6e6c4ea4b3a0406274e8632cb7340
kirandash <thekirankumardash@gmail.com>
2016-10-08 16:40:24 +0530

    05 - 04 Implement Filters

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -14,7 +14,7 @@
 					title: "First Item",
 					price: "1,00,000",
 					description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ac arcu sit amet lacus ullamcorper tincidunt. Nulla placerat ex nulla, ut tempor leo lacinia non. Morbi pharetra sagittis ante. Sed id ornare enim. Aenean dictum dignissim justo, nec convallis sapien aliquet a. Maecenas in justo vulputate, sagittis lectus facilisis, consectetur velit. Proin quis viverra lorem. Donec tempus ante vitae lacus feugiat tempus. Praesent et hendrerit turpis. Etiam non nisi felis.",
-					posted: "2016-05-11",
+					posted: "2016-12-11",
 					contact: {
 						name: 'Kiran Dash',
 						phone: '+91-8121171321',
