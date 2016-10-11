4d99c02f61774bef181d0c645e89704982ac9bea
kirandash <thekirankumardash@gmail.com>
2016-10-11 13:55:38 +0530

    06 - 03 Switc to Services

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -6,12 +6,12 @@
 		.module("ngClassifieds") // reference existing module - not creating new module
 
 		// scope is the special object of our controller which acts as glue betweeen contoller and view
-		.controller("classifiedsCtrl", function($scope, $http){
+		.controller("classifiedsCtrl", function($scope, $http, classifiedsFactory){
 			
 			// get method has promises, once the task is done then the code inside it will execute. any code outside will run 
 			// independent of promises
 
-			$http.get('data/classifieds.json').then(function(classifieds){
+			classifiedsFactory.getClassifieds().then(function(classifieds){
 				// property defined				
 				// console.log(data);
 				$scope.classifieds = classifieds.data;
