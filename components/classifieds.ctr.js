458022e2f9dcb095bc293cdf42a9512b9d1bc2fe
kirandash <thekirankumardash@gmail.com>
2016-10-11 15:05:27 +0530

    06 - 01 Putting in the Sidenav

--- components/classifieds.ctr.js
+++ components/classifieds.ctr.js
@@ -6,7 +6,7 @@
 		.module("ngClassifieds") // reference existing module - not creating new module
 
 		// scope is the special object of our controller which acts as glue betweeen contoller and view
-		.controller("classifiedsCtrl", function($scope, $http, classifiedsFactory){
+		.controller("classifiedsCtrl", function($scope, $http, classifiedsFactory, $mdSidenav){
 			
 			// get method has promises, once the task is done then the code inside it will execute. any code outside will run 
 			// independent of promises
@@ -16,6 +16,16 @@
 				// console.log(data);
 				$scope.classifieds = classifieds.data;
 			});
+
+			$scope.openSidebar = function() {
+				// md-component-id passed to mdsidenav
+				$mdSidenav('left').open();
+			}
+
+			$scope.closeSidebar = function() {
+				// md-component-id passed to mdsidenav
+				$mdSidenav('left').close();
+			}
 
 		}); // controller defined - MVC
 })();
