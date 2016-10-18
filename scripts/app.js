de3a50605ffd3d6cf8c199c8f3b4efa4465b1243
kirandash <thekirankumardash@gmail.com>
2016-10-18 07:33:07 +0530

    08 - 03 Switch to Controller As

--- scripts/app.js
+++ scripts/app.js
@@ -8,7 +8,8 @@ angular.module("ngClassifieds", ["ngMaterial", "ui.router"]) // creating new mod
 	   		$stateProvider
 	   			.state('stateone', {
 	   				url: '/stateone',
-	   				template: '<h1>State One</h1>'
+	   				template: '<h1>State One {{ stateone.message }}</h1>', // alias . notation controller for larger project
+	   				controller: 'stateOneCtrl as stateone' // alias for controller
 	   			})
 	   			.state('statetwo', {
 	   				url: '/statetwo',
@@ -17,7 +18,14 @@ angular.module("ngClassifieds", ["ngMaterial", "ui.router"]) // creating new mod
 	   			.state('statetwo.more', {
 	   				url: '/more',
 	   				template: '<p>This is a deeper nested state</p>'
-	   			})
+	   			});
+	   })
+
+	   .controller('stateOneCtrl', function(){
+	   		// capture variable - vm = view model
+	   		// remove $scope to avoid scope soup for larger projects in angular
+	   		var vm = this;
+	   		vm.message = "Hey from state one";
 	   })
 	   // custom directive
 	   .directive("helloWorld", function(){
